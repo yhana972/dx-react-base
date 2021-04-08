25e30c058b15cc215ba1ebfc0f3ac9c4c1453be0
dundu-slowalk <dundu@slowalk.co.kr>
2021-04-08 15:39:37 +0900

    3.1 setState

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ class App extends React.Component{
   };
 
   add = () => {
-    console.log("add");
+    this.setState(current => ({count : current.count + 1 }));
   }
 
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({count : current.count - 1}));
   }
   render(){
     return(
