2bf0eb229461d07d42d0e0dfc74a7e73c14245a4
dundu-slowalk <dundu@slowalk.co.kr>
2021-04-08 18:47:49 +0900

    3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import './App.css';
-import PropTypes from "prop-types";
 import React, { Component } from 'react';
 
 class App extends React.Component{
+
   state = {
-    count: 0
+    isLoading: true
   };
 
-  add = () => {
-    this.setState(current => ({count : current.count + 1 }));
-  }
-
-  minus = () => {
-    this.setState(current => ({count : current.count - 1}));
-  }
-
   componentDidMount(){
-    console.log("렌더링 완료!");
+    setTimeout(()=>{
+      this.setState({isLoading: false});
+    }, 6000);
   }
-  componentDidUpdate(){
-    console.log("업데이트 완료!");
-  }
-  componentWillUnmount(){
-    console.log("렌더링 종료");
-  }
+
   render(){
-    console.log("렌더링중");
+    const{ isLoading } = this.state;
     return(
       <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {
+          isLoading ? "Loading..." : "We are ready"
+        }
       </div>
     );
   }
