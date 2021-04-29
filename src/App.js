f0d376dbea2c1e1663a5bf428388274be6616415
dundu-slowalk <dundu@slowalk.co.kr>
2021-04-29 15:44:36 +0900

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home"
+import Navigation from "./components/Navigation"
 
 function App(){
-  return <span>something</span>;
+  return(
+    <BrowserRouter>
+      <Navigation/>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About}/>
+    </BrowserRouter>
+  );
 }
 
 export default App;
