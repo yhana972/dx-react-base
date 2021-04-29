2155514e23da5eaebaa4c35bb14099e3446c4832
dundu-slowalk <dundu@slowalk.co.kr>
2021-04-29 12:17:12 +0900

    github page

--- src/Movies.js
+++ src/Movies.js
@@ -13,7 +13,7 @@ function Movie({year, title, summary, poster, genres}){
         {genres.map((genre, index) =>
         <li key={index} className="genres__genre">{genre}</li>)}
       </ul>
-      <p className="movie__summary">{summary}</p>
+      <p className="movie__summary">{summary.slice(0, 180)}...</p>
     </div>
   </div>);
 }
