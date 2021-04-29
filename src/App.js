6478d0ee5e6fb5b09bb8cb3496da88fcc305db4e
dundu-slowalk <dundu@slowalk.co.kr>
2021-04-29 16:44:25 +0900

    movie-app done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {BrowserRouter, Route} from "react-router-dom";
 import About from "./routes/About";
-import Home from "./routes/Home"
-import Navigation from "./components/Navigation"
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 function App(){
   return(
@@ -10,6 +11,7 @@ function App(){
       <Navigation/>
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About}/>
+      <Route path="/movie/:id" component={Detail} />
     </BrowserRouter>
   );
 }
